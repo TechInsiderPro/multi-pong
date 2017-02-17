98b0ccd52ee3d6422b71a26891936ba9326b5176
Unknown <techinsiderpro@gmail.com>
2017-02-17 09:56:13 -0500

    Created basic World class
    World
    -vars : entities

---
+++ src/World.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Programming on 2/17/2017.
+ */
+public class World
+{
+    public List<Entity> entities;
+
+    public World()
+    {
+        entities = new ArrayList<>();
+    }
+}
