cd7d6eb4411123e82a2517850604f87a7c0c9441
Unknown <techinsiderpro@gmail.com>
2017-02-17 10:05:23 -0500

    Implemented Updatable in World and Entity

--- src/Entity.java
+++ src/Entity.java
@@ -1,7 +1,7 @@
 /**
  * Created by Programming on 2/17/2017.
  */
-public class Entity
+public class Entity implements Updatable
 {
     public double x, y, vx, vy;
 
@@ -9,5 +9,12 @@ public class Entity
     {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public void update(long delta)
+    {
+        x += vx;
+        y += vy;
     }
 }
--- src/World.java
+++ src/World.java
@@ -4,12 +4,18 @@ import java.util.List;
 /**
  * Created by Programming on 2/17/2017.
  */
-public class World
+public class World implements Updatable
 {
     public List<Entity> entities;
 
     public World()
     {
         entities = new ArrayList<>();
+    }
+
+    @Override
+    public void update(long delta)
+    {
+        entities.forEach((e) -> e.update(delta));
     }
 }
